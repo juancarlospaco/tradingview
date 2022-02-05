17cc8a37d0fdac585e78b5a06f100f8c97091ed7
luisacosta <luisacosta828@gmail.com>
2022-02-04 22:51:24 -0400

    added new examples

--- examples/example.nim
+++ examples/example.nim
@@ -1,6 +1,17 @@
-import tradingview
+import ../src/tradingview
 
-const indicators = { low(Indicators) .. high(Indicators) }
-var trading = newTradingView("BTCUSDT", 10_000, indicators = indicators)
+var trading = newTradingView("1INCHUSDT_PREMIUM", 10_000, indicators = getIndicators())
+echo "1INCHUSDT_PREMIUM\n", $trading.getAnalysis().summary.recommendation & "\n"
 
-echo "BTCUSDT\n", trading.getAnalysis().summary.recommendation
+trading = newTradingView("BTCUSDT", 10_000, indicators = getIndicators())
+echo "BTCUSDT\n", $trading.getAnalysis().summary.recommendation & "\n"
+
+trading = newTradingView("AAVEBTC", 10_000, indicators = getIndicators())
+echo "AAVEBTC\n", $trading.getAnalysis().summary.recommendation & "\n"
+
+trading = newTradingView("ACMUSD", 10_000, indicators = getIndicators())
+echo "ACMUSD\n", $trading.getAnalysis().summary.recommendation & "\n"
+
+trading = newTradingView("1000SHIBUSDTPERP", 10_000, indicators = getIndicators())
+echo "1000SHIBUSDTPERP\n", $trading.getAnalysis().summary.recommendation & "\n"
+
